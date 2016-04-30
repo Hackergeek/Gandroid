2990a846a63c6323a70326edf466ed5c8c5d64b3
Hackergeek <1224085894@qq.com>
2016-04-30 16:32:22 +0800

    PhotoIntentActivity 2016/4/30

---
+++ PhotoIntentActivity/app/src/main/java/skyward/com/photointentactivity/AlbumStorageDirFactory.java
@@ -0,0 +1,7 @@
+package skyward.com.photointentactivity;
+
+import java.io.File;
+
+abstract class AlbumStorageDirFactory {
+	public abstract File getAlbumStorageDir(String albumName);
+}
---
+++ PhotoIntentActivity/readme.md
@@ -0,0 +1,4 @@
+##<a href="http://hukai.me/android-training-course-in-chinese/multimedia/camera/index.html">拍照</a>
+###1.简单的拍照
+###2.简单的录像
+###3.控制相机硬件
